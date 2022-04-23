dec7b221563145be65af23f5cc1b24fd7f13a5ae
Omar Assadi <omar_assadi@external.mckinsey.com>
2022-04-23 17:40:51 +0200

    adds integration test on mobile screen

--- cypress/integration/sign-up.integration.test.js
+++ cypress/integration/sign-up.integration.test.js
@@ -1,17 +1,32 @@
 describe('Sign Up', () => {
-  beforeEach(() => {
-    cy.viewport('macbook-15');
+
+  const testSignUp = () => {
+    it('User should be able to sign up', () => {
+      cy.visit('http://localhost:7000/');
+      cy.findByText('Sign Up').click();
+      cy.location('pathname').should('eq', '/sign-up');
+      cy.findByPlaceholderText('Full Name').type('John Doe');
+      cy.findByPlaceholderText('Email').type('email@gmail.com');
+      cy.findByPlaceholderText('Enter Password').type('password');
+      cy.get('button[type=submit]').click();
+      cy.location('pathname').should('eq', '/create-video');
+    });
+  };
+
+  context('Mobile', () => {
+    beforeEach(() => {
+      cy.viewport('iphone-se2');
+    });
+
+    testSignUp();
   });
 
-  it('User should be able to sign up', () => {
-    cy.visit('http://localhost:7000/');
-    cy.findByText('Sign Up').click();
-    cy.location('pathname').should('eq', '/sign-up');
-    cy.findByPlaceholderText('Full Name').type('John Doe');
-    cy.findByPlaceholderText('Email').type('email@gmail.com');
-    cy.findByPlaceholderText('Enter Password').type('password');
-    cy.get('button[type=submit]').click();
-    cy.location('pathname').should('eq', '/create-video');
+  context('Desktop', () => {
+    beforeEach(() => {
+      cy.viewport('macbook-15');
+    });
+
+    testSignUp();
   });
 });
 
